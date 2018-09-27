c70d037a79f68d0a7c2bbf17f15ea66454ca9be3
okhattab <okhattab@cmu.edu>
2018-09-27 17:37:55 +0300

    Minor modifications for clarity

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,13 @@
 from __future__ import print_function
+import os
 import sys
 import numpy as np
 
-# Set these appropriate
-path_prefix = sys.argv[1].strip()
-part_idx = sys.argv[2].strip()
 
-# VAD Parameters (also, see basic normalization below)
-VAD_THRESHOLD = -80   # if a frame has no filter that exceeds this threshold, it is assumed silent and removed
-VAD_MIN_NFRAMES = 150  # if a filtered utterance is shorter than this after VAD, the full utterance is retained
+## VAD Parameters ##
+VAD_THRESHOLD = -80     # if a frame has no filter that exceeds this threshold, it is assumed silent and removed
+VAD_MIN_NFRAMES = 150   # if a filtered utterance is shorter than this after VAD, the full utterance is retained
 
-
-assert(path_prefix)
-assert(path_prefix)
-assert(part_idx in map(lambda x: str(x), list(range(1, 7))) or part_idx in ["dev", "test"])
 assert(VAD_THRESHOLD >= -100.0)
 assert(VAD_MIN_NFRAMES >= 1)
 
@@ -32,16 +26,23 @@ def normalize(utterance):
     return np.float16(utterance)
 
 
-npz = np.load(path_prefix + str(part_idx) + ".npz", encoding='latin1')
+if __name__ == "__main__":
+    if len(sys.argv) < 3 or sys.argv[2] not in list(map(str, range(1, 7))) + ["dev", "test"]:
+        print("Usage:", sys.argv[0], "<path to npz files>", "<chunk among {1, 2, .., 6, dev, test}>")
+        exit(0)
 
-if part_idx == "dev":
-    enrol, test = bulk_VAD(npz['enrol']), bulk_VAD(npz['test'])
-    np.savez(path_prefix + str(part_idx) + ".preprocessed.npz", enrol=enrol, test=test, trials=npz['trials'], labels=npz['labels'])
+    path, part = sys.argv[1], sys.argv[2]
+    input_path = os.path.join(path, part + ".npz")
+    output_path = os.path.join(path, part + ".preprocessed.npz")
 
-elif part_idx == "test":
-    enrol, test = bulk_VAD(npz['enrol']), bulk_VAD(npz['test'])
-    np.savez(path_prefix + str(part_idx) + ".preprocessed.npz", enrol=enrol, test=test, trials=npz['trials'])
+    npz = np.load(input_path, encoding='latin1')
 
-else:
-    feats, targets = bulk_VAD(npz['feats']), npz['targets']
-    np.savez(path_prefix + str(part_idx) + ".preprocessed.npz", feats=feats, targets=targets)
+    if part == "dev":
+        np.savez(output_path, enrol=bulk_VAD(npz['enrol']), test=bulk_VAD(npz['test']), trials=npz['trials'],
+                 labels=npz['labels'])
+
+    elif part == "test":
+        np.savez(output_path, enrol=bulk_VAD(npz['enrol']), test=bulk_VAD(npz['test']), trials=npz['trials'])
+
+    else:
+        np.savez(output_path, feats=bulk_VAD(npz['feats']), targets=npz['targets'])
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import os
 from tqdm import tqdm
 import numpy as np
 from scipy.optimize import brentq
@@ -7,29 +8,19 @@ from scipy.interpolate import interp1d
 from sklearn.metrics import roc_curve
 
 
-def EER(labels, scores):
-    """
-    Computes EER (and threshold at which EER occurs) given a list of (gold standard) True/False labels
-    and the estimated similarity scores by the verification system (larger values indicates more similar)
-    Sources: https://yangcha.github.io/EER-ROC/ & https://stackoverflow.com/a/49555212/1493011
-    """
-    fpr, tpr, thresholds = roc_curve(labels, scores, pos_label=True)
-    eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
-    thresh = interp1d(fpr, thresholds)(eer)
-    return eer, thresh
-
-
-def train_load(prefix, parts, verbose=False):
+def train_load(path, parts):
     """
     Example Usage: train_load("/path/to/training/files/", range(1,4)) to load npz files 1-3 inclusive.
     Stacks all the utterances and re-numbers the speaker IDs into a dense integer domain.
+    NOTE: Expects the npz files to have already been preprocessed (i.e., in terms of VAD and normalization) using
+          the preprocess.py script
     """
 
     features = []
     speakers = []
 
     for p in tqdm(parts):
-        npz = np.load(prefix + str(p) + ".preprocessed.npz", encoding='latin1')
+        npz = np.load(os.path.join(path, str(p) + ".preprocessed.npz"), encoding='latin1')
 
         features.append(npz['feats'])
         speakers.append(npz['targets'])
@@ -43,9 +34,9 @@ def train_load(prefix, parts, verbose=False):
     return features, speakers, nspeakers
 
 
-def dev_load(path, verbose=False):
+def dev_load(path):
     """
-    Given path to dev.preprocessed.npz file, loads and returns:
+    Given path to the dev.preprocessed.npz file, loads and returns:
     (1) Dev trials list, where each item is [enrollment_utterance_idx, test_utterance_idx]
     (2) Dev trials labels, where each item is True if same speaker (and False otherwise)
     (3) (Dev) Enrollment array of utterances
@@ -55,17 +46,16 @@ def dev_load(path, verbose=False):
     assert '.preprocessed.npz' in path
 
     data = np.load(path, encoding='latin1')
-    enrol = data['enrol']
-    test = data['test']
+    enrol, test = data['enrol'], data['test']
 
     print("Loaded dev data.")
 
     return data['trials'], data['labels'], enrol, test
 
 
-def test_load(path, verbose=False):
+def test_load(path):
     """
-    Given path to test.preprocessed.npz file, loads and returns:
+    Given path to the test.preprocessed.npz file, loads and returns:
     (1) Test trials list, where each item is [enrollment_utterance_idx, test_utterance_idx]
     (2) (Test) Enrollment array of utterances
     (3) (Test) Test array of utterances
@@ -74,12 +64,23 @@ def test_load(path, verbose=False):
     assert '.preprocessed.npz' in path
 
     data = np.load(path, encoding='latin1')
-    enrol = data['enrol']
-    test = data['test']
+    enrol, test = data['enrol'], data['test']
 
     print("Loaded test data.")
 
     return data['trials'], enrol, test
+
+
+def EER(labels, scores):
+    """
+    Computes EER (and threshold at which EER occurs) given a list of (gold standard) True/False labels
+    and the estimated similarity scores by the verification system (larger values indicates more similar)
+    Sources: https://yangcha.github.io/EER-ROC/ & https://stackoverflow.com/a/49555212/1493011
+    """
+    fpr, tpr, thresholds = roc_curve(labels, scores, pos_label=True)
+    eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+    thresh = interp1d(fpr, thresholds)(eer)
+    return eer, thresh
 
 
 def densify_speaker_IDs(speakers):
